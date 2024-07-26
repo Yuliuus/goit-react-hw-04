4ec7a66c16300cc5b69c6634dcb06a9ab94d8ae8
Yuliuus <yuliiamarkovska@gmail.com>
2024-07-26 12:11:48 +0300

    save to local storage fixed

---
+++ src/components/App/App.jsx
@@ -0,0 +1,7 @@
+import SearchBar from "../SearchBar/SearchBar";
+
+function App() {
+  return <SearchBar />;
+}
+
+export default App;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,16 @@
+
+export default function SearchBar({ onSubmit }) {
+  return (
+    <header>
+  <form>
+    <input
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+    <button type="submit">Search</button>
+  </form>
+</header>
+  );
+}
